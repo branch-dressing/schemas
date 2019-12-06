964efc9fac37d4cf085dd0c7c50702bd25186626
joelpdurham <joeldur@gmail.com>
2019-12-06 12:53:50 -0800

    wrote test for write JSON

--- starter-code/_tests_/fs-function.test.js
+++ starter-code/_tests_/fs-function.test.js
@@ -1,11 +1,13 @@
 const fs = require('fs').promises;
 const {
-    mkdirp
+    mkdirp,
+    writeJSON
 } = require('../lib/fs-function.js');
 
 jest.mock('fs', () => ({
     promises: {
-        mkdir: jest.fn(() => Promise.resolve())
+        mkdir: jest.fn(() => Promise.resolve()),
+        writeFile: jest.fn(() => Promise.resolve())
     }
 }));
 
@@ -14,6 +16,19 @@ describe('fs-functions test', () => {
         return mkdirp('./some/example/test/dirs')
             .then(() => {
                 expect(fs.mkdir).toHaveBeenCalledWith('./some/example/test/dirs', { recursive: true });
+            });
+    });
+
+    it('will write a file', () => {
+        return writeJSON('./some/example/test/dirs', {
+            one: 'one',
+            two: 2
+        })
+            .then(() => {
+                expect(fs.writeJSON).toHaveBeenCalledWith('./some/example/test/dirs', JSON.stringify({ 
+                    one: 'one',
+                    two: 2
+                }));
             });
     });
 });
